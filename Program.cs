1c7fc5155781a2f85abcda7888f441cdc3ae6b9b
Mateusz Wiącek <68820849+MarioPeperoni@users.noreply.github.com>
2024-06-09 22:59:44 +0200

    Fix for native app

--- Program.cs
+++ Program.cs
@@ -53,22 +53,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         x.Authority = builder.Configuration["Clerk:Authority"];
         x.TokenValidationParameters = new TokenValidationParameters()
         {
-            // Disable audience validation as we aren't using it
             ValidateAudience = false,
             NameClaimType = ClaimTypes.NameIdentifier
-        };
-        x.Events = new JwtBearerEvents()
-        {
-            // Additional validation for AZP claim
-            OnTokenValidated = context =>
-            {
-                var azp = context.Principal?.FindFirstValue("azp");
-                // AuthorizedParty is the base URL of your frontend.
-                if (string.IsNullOrEmpty(azp) || !azp.Equals(builder.Configuration["Clerk:AuthorizedParty"]))
-                    context.Fail("AZP Claim is invalid or missing");
-
-                return Task.CompletedTask;
-            }
         };
     });
 
